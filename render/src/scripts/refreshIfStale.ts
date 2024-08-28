762d628729803f3a3a984b7f337f3736660d0dbf
Max Goodhart <c@chromakode.com>
2024-08-27 23:25:33 -0700

    Refresh page when it becomes visible if stale

---
+++ render/src/scripts/refreshIfStale.ts
@@ -0,0 +1,18 @@
+import { navigate } from 'astro:transitions/client'
+import { addMinutes } from 'date-fns'
+
+const REFRESH_INTERVAL_MIN = 15
+
+document.addEventListener('visibilitychange', () => {
+  const now = Date.now()
+  const lastUpdateTime =
+    document.querySelector<HTMLTimeElement>('.updated')?.dateTime
+
+  if (
+    document.visibilityState === 'visible' &&
+    lastUpdateTime != null &&
+    now > addMinutes(lastUpdateTime, REFRESH_INTERVAL_MIN).getTime()
+  ) {
+    navigate('')
+  }
+})
